d51f2b1798140a96fdb35994c78f82b3f3112361
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-10-22 18:32:11 +0300

    created user class

---
+++ user.py
@@ -0,0 +1,24 @@
+import unittest
+from credentials import Credentials 
+
+class User:
+    '''
+    class that generates new instance of user
+
+    '''
+
+    user_list = []  # it will save new users
+
+    def __init__(self, user_name, user_password):
+        '''
+        __init__ method defines the properties of user object
+        Args: 
+            user_name = username of user
+            user_password =password user
+        '''
+
+        self.user_name = user_name
+    
+
+
+
